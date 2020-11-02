7f0ec6bd4127aae784490485c21828008d62e125
DcDeneme <73835308+DcDeneme@users.noreply.github.com>
2020-11-02 16:56:38 +0300

    Update nitro.js

--- nitro.js
+++ nitro.js
@@ -34,7 +34,7 @@ function spamla(){
 	message.channel.send("https:/"+"/discord.gift/" + rasteleSembol(16,'0aA'));
 }
 
-	if (message.channel.id = "ID NUMARASI"){
+	if (message.channel.id = "757897284766662676"){
 		if(message.content.startsWith("hrr!nitro ")){
 		try{
 		var veri = message.content.toString();
@@ -58,6 +58,6 @@ function spamla(){
 
 
 });
-client.login("TOKENİNİZ");
+client.login("159985870458322944");
     
     
